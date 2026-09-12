cf2685aa91412979a2b2d0ab0ff159a0a661e8fd
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-09-12 18:20:50 +0900

    fix: どこからも呼ばれていないコード削除

--- code/f1872707871b2c/solution/src/model/store/isInputStep.ts
+++
@@ -1,9 +0,0 @@
-import type { InputStep } from "./types";
-import { InputStepList } from "./const";
-
-export const isInputStep = (value: unknown): value is InputStep => {
-  if (typeof value !== "string") {
-    return false;
-  }
-  return InputStepList.some((step) => step === value);
-};
